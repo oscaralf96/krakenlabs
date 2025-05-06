282c9f5247eed040b2db0cb14cf09922c5a1ef33
Oscar Gonzalez <racsogonzalez30@gmail.com>
2025-05-06 12:47:55 -0600

    base, local, dev settings configurations

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+    ENVIRONMENT = os.getenv('ENVIRONMENT')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"api.settings.{ENVIRONMENT}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
