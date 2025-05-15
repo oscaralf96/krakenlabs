79a4e609bbe36e79c624edaae8e6dfe0aef8d6eb
Oscar Gonzalez <racsogonzalez30@gmail.com>
2025-05-15 01:00:11 -0600

    jwt

--- backend/core_apps/users/urls.py
+++ backend/core_apps/users/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, WebsiteCreateView, test_login_view
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', test_login_view(), name='login'),
+    path('login/', test_login_view, name='login'),
     path('websites/', WebsiteCreateView.as_view(), name='create-website'),
 ]
