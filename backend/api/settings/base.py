63933a81b09b737129ccc754e8bd37f1ed699a17
Oscar Gonzalez <racsogonzalez30@gmail.com>
2025-05-15 01:14:50 -0600

    jwt

--- backend/api/settings/base.py
+++ backend/api/settings/base.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "core_apps.middleware.DisableCSRFMiddleware",
+    "core_apps.users.middleware.DisableCSRFMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
